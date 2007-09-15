a625e8a8d3564be651fbf1bbd722a777452046ab
Ted Percival <ted@midg3t.net>
2007-09-15 18:53:11 +1000

    Uniform copyright info - Including copyright holders names in each source file - Removing some instances of avahi in the copyright notices

--- natmap.h
+++ natmap.h
@@ -1,25 +1,23 @@
 #ifndef foonatmaphfoo
 #define foonatmaphfoo
 
-/* $Id$ */
-
 /***
-  This file is part of avahi.
+  This file is part of Stallone.
+  Copright 2007  Ted Percival <ted@midg3t.net>
  
-  avahi is free software; you can redistribute it and/or modify it
-  under the terms of the GNU Lesser General Public License as
-  published by the Free Software Foundation; either version 2.1 of the
-  License, or (at your option) any later version.
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
  
-  avahi is distributed in the hope that it will be useful, but WITHOUT
-  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
-  Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
  
   You should have received a copy of the GNU Lesser General Public
-  License along with avahi; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
-  USA.
+  License along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 ***/
 
 #include <stdint.h>
