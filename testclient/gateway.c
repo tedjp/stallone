8fc12f8ef3aead5cc28134f1fd6718d61c3a9e75
Ted Percival <ted@midg3t.net>
2010-03-01 18:38:41 -0700

    Fix parsing /proc/net/route when interface is not 4 chars long

--- testclient/gateway.c
+++ testclient/gateway.c
@@ -62,6 +62,66 @@ static char *get_line(char buf[], size_t buflen, FILE *stream) {
 }
 
 /**
+ * Parse the given line looking for a gateway definition.
+ * If the gateway flag is set, the address is stored in *addr
+ * and the function returns 1. If no gateway is found, it returns
+ * 0 and the content of *addr is undefined.
+ *
+ * The input line will be modified during the search process.
+ */
+static int find_gateway(char *line, struct in_addr *addr) {
+    unsigned long int flags = 0;
+    char *endptr;
+    const char *addrstr = NULL, *flagstr = NULL;
+    unsigned int field = 1;
+    union {
+        struct in_addr sin_addr;
+        unsigned long numeric;
+    } address;
+
+    if (!line || !addr)
+        return 0;
+
+    while ((line = strtok(field == 1 ? line : NULL, "\t "))) {
+        if (field == 3)
+            addrstr = line;
+        else if (field == 4) {
+            flagstr = line;
+            break;
+        }
+        ++field;
+    }
+
+    if (!addrstr || !flagstr) {
+        daemon_log(LOG_WARNING, "%s: Did not find %s field\n",
+                __FUNCTION__, (addrstr == NULL) ? "gateway" : "flags");
+        return 0;
+    }
+
+    flags = strtoul(flagstr, &endptr, 16);
+    if (endptr == flagstr || *endptr != '\0') {
+        daemon_log(LOG_WARNING, "%s: Could not parse flag string \"%s\"",
+                __FUNCTION__, flagstr);
+        return 0;
+    }
+
+    if ((flags & RTF_GATEWAY) && (flags & RTF_UP)) { /* Valid gateway */
+        address.numeric = strtoul(addrstr, &endptr, 16);
+        if (endptr == addrstr || *endptr != '\0') {
+            daemon_log(LOG_ERR, "%s: Could not parse address string \"%s\"",
+                    __FUNCTION__, addrstr);
+            return 0;
+        }
+
+        *addr = address.sin_addr;
+
+        return 1;
+    }
+
+    return 0;
+}
+
+/**
  * Gets the default route (gateway) that NAT-PMP packets should be sent
  * to.
  * Returns -1 on error, 0 on success.
@@ -69,15 +129,7 @@ static char *get_line(char buf[], size_t buflen, FILE *stream) {
 int avahi_natpm_get_gateway(struct in_addr *addr) {
     FILE *froute = NULL;
     char line[LINE_BUF_LEN];
-    /* Offsets into the line to find the string representations of
-     * things we are interested in. */
-    const size_t ADDRESS_OFFSET = 14, FLAGS_OFFSET = 23;
     int result = -1;
-    union {
-        struct in_addr sin_addr;
-        unsigned long numeric;
-    } address;
-        
 
     froute = fopen(PROC_NET_ROUTE, "r");
     if (!froute) {
@@ -95,43 +147,9 @@ int avahi_natpm_get_gateway(struct in_addr *addr) {
 
     /* Look for the gateway line */
     while (get_line(line, sizeof(line), froute)) {
-        char *flagstr = line + FLAGS_OFFSET;
-        unsigned long int flags;
-        char *endptr;
-
-        if (strlen(line) < FLAGS_OFFSET || strlen(line) < ADDRESS_OFFSET) {
-            daemon_log(LOG_WARNING, "%s: Short line encountered while parsing %s",
-                    __FUNCTION__, PROC_NET_ROUTE);
-            continue;
-        }
-
-        /* The flags field is 4 digits long. */
-        flagstr[4] = '\0';
-
-        flags = strtoul(flagstr, &endptr, 16);
-        if (endptr == flagstr || *endptr != '\0') {
-            daemon_log(LOG_WARNING, "%s: Could not parse flag string \"%s\"",
-                    __FUNCTION__, flagstr);
-            continue;
-        }
-
-        if ((flags & RTF_GATEWAY) && (flags & RTF_UP)) { /* Valid gateway */
-            char *addrstr = line + ADDRESS_OFFSET;
-
-            /* Address field is 8 digits long. */
-            addrstr[8] = '\0';
-
-            address.numeric = strtoul(addrstr, &endptr, 16);
-            if (endptr == addrstr || *endptr != '\0') {
-                daemon_log(LOG_ERR, "%s: Could not parse address string \"%s\"",
-                        __FUNCTION__, addrstr);
-                continue;
-            }
-
-            *addr = address.sin_addr;
-
+        if (find_gateway(line, addr)) {
             result = 0;
-            goto finish;
+            break;
         }
     }
 
